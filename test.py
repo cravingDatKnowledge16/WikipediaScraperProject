09ff9e4ad2236c23ef6c19e350633864bf09ffc7
cravingDatKnowledge16 <ynneb16@gmail.com>
2023-11-23 23:10:45 +0100

    -

--- test.py
+++ test.py
@@ -13,7 +13,7 @@ objInd = [x for x in enumerate(objKeys)]
 
 #print(dictToList(obj))
 
-print(range(len([1,4,2,5])))
+#print(range(len([1,4,2,5])))
 
 def test(value):
     return ("/wiki/" in value) or ("Datei:" not in value) or ("Hilfe:" not in value) or ("Wikipedia:" not in value) or ("Spezial:" not in value) or ("https:" not in value)
@@ -22,8 +22,16 @@ def test2(value):
     bannedWords = ["t","i","o"]
     return [el not in value for el in bannedWords].count(False) != 0
 
-print(test2("tno"))
+#print(test2("tno"))
 
+
+
+
+def areObjectsInObject(value,checkList):
+    #returns True, if an object 
+    return [word not in value for word in checkList].count(False) != 0
+
+print(areObjectsInObject("de.wikipedia.org/wiki/Universum",["Datei:","Hilfe:","Wikipedia:","Spezial:","https://"]))
 
 list = [[0,1],[89,5],[20,7]]
 
